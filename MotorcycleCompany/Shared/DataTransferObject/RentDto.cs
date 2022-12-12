d401dcf4b038a7a0ab0f4b568a7eee8f0cbeca6c
unknown <bafigueroa71@misena.edu.co>
2022-12-12 14:08:03 -0600

    Changes 5

---
+++ MotorcycleCompany/Shared/DataTransferObject/RentDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObject
+{
+   public record RentDto(string Mat, string NameAgency, string DNIClient);
+}
